195316d48ebd6f7e3f20f3b5d6615caf71ec4394
shon buchbinder <shonb65@gmail.com>
2023-05-17 23:40:07 +0300

    added controller guest init

---
+++ GuestService/src/GuestService/Controllers/GuestsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagemnt.GuestService.Controllers
+{
+    [ApiController]
+    [Route("guests")]
+    public class GuestsController : ControllerBase
+    {
+
+    }
+}
