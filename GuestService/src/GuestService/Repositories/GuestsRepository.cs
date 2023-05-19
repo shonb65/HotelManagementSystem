177f77934306a2a14612d3f3931d64ad941366c8
shon buchbinder <shonb65@gmail.com>
2023-05-20 01:51:01 +0300

    added some CRUD methods for the db interact

--- GuestService/src/GuestService/Repositories/GuestsRepository.cs
+++ GuestService/src/GuestService/Repositories/GuestsRepository.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Transactions;
 using MongoDB.Driver;
 using HotelManagemnt.GuestService.Entities;
+using System.Threading.Tasks;
 
 namespace HotelManagemnt.GuestService.Repositories
 {
@@ -15,6 +19,44 @@ namespace HotelManagemnt.GuestService.Repositories
             var mongoClient = new MongoClient("mongodb://localhost:27017");
             var database = mongoClient.GetDatabase("Guest");
             dbCollection = database.GetCollection<Guest>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<Guest>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<Guest> GetAsync(Guid id)
+        {
+            FilterDefinition<Guest> filter = filterBuilder.Eq(entity => entity.Id, id);
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(Guest entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await dbCollection.InsertOneAsync(entity);
+        }
+
+        public async Task UpdateAsync(Guest entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            FilterDefinition<Guest> filter = filterBuilder.Eq(existingEntity => existingEntity.Id, entity.Id);
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            FilterDefinition<Guest> filter = filterBuilder.Eq(entity => entity.Id, id);
+            await dbCollection.DeleteOneAsync(filter);
         }
     }
 }
